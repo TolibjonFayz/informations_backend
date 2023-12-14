73e42d718bc80773bf7e0cdf0ec4e39a9745cbdf
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2023-12-14 18:43:33 +0500

    Mail is edited

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -6,7 +6,7 @@ import { Admin } from '../admin/model/admin.model';
 export class MailService {
   constructor(private mailerservice: MailerService) {}
   async sendAdminConfrmation(admin: Admin): Promise<void> {
-    const url = `${process.env.API_HOST}3000/api/admin/activate/${admin.unique_id}`;
+    const url = `https://informationsbackend-production.up.railway.app/api/admin/activate/${admin.unique_id}`;
     await this.mailerservice.sendMail({
       to: admin.email,
       subject: '"Malumotlar" saytiga xush kelibsiz! Emailingizni tasdqilang!',
